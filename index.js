5f0a23c1c13e022cebedbfee96900947b9795763
Raihan <aboraihan14@gmail.com>
2024-12-05 17:54:19 +0600

    delete added

--- index.js
+++ index.js
@@ -45,6 +45,76 @@ async function run() {
 
 
 
+        //! user post ...........................................................
+        app.post('/apply-visa', async (req, res) => {
+            const application = req.body;
+            const result = await client.db('visa').collection('applications').insertOne(application);
+            res.send(result);
+        });
+        //   !  2nd.................................... 
+
+        app.get('/my-visa-applications', async (req, res) => {
+            const email = req.query.email;
+            try {
+                const applications = await client
+                    .db("visa")
+                    .collection("applications")
+                    .find({ email })
+                    .toArray();
+                res.send(applications);
+            } catch (error) {
+                res.status(500).send({ message: 'Error fetching applications' });
+            }
+        });
+
+        // DELETE route for removing an application
+
+
+        app.delete('/cancel-application/:id', async (req, res) => {
+            const id = req.params.id;
+            try {
+                const result = await client
+                    .db("visa")
+                    .collection("applications")
+                    .deleteOne({ _id: new ObjectId(id) }); // Ensure correct conversion to ObjectId
+
+                if (result.deletedCount === 1) {
+                    res.send({ message: 'Application canceled successfully' });
+                } else {
+                    res.status(404).send({ message: 'Application not found' });
+                }
+            } catch (error) {
+                console.error('Error deleting application:', error);
+                res.status(500).send({ message: 'Error canceling application' });
+            }
+        });
+
+
+
+
+        // ! 3rd 
+
+
+
+        app.delete('/delete-visa/:id', async (req, res) => {
+
+            const { id } = req.params;
+
+            const query = { _id: new ObjectId(id) }
+            const result = await visa.deleteOne(query)
+            res.send(result)
+
+
+        });
+
+        // !4th 
+
+
+
+
+
+
+
 
 
     } catch (error) {
